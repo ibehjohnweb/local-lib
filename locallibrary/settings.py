22aec682a2b69fd7703ce8075680bfce65ba7ed8
Ibeh John <ibehjohnweb@gmail.com>
2022-03-17 18:01:58 +0800

    Allowed Host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'johnlib.herokuapp.com/', '127.0.0.1',
+]
+
 
 
 # Application definition
@@ -135,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag"
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag")
+# 'django-insecure-n&=h+)$f1y2d45rm()43a5r&i4)#^-r#j-%+31l2@hwyli&*7n'
 # Heroku: Update database configuration from $DATABASE_URL.
 
 db_from_env = dj_database_url.config(conn_max_age=500)
